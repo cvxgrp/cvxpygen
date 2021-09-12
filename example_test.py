48e6cabb879d795490b98832c75081ea37451a96
maximilianschaller <79449026+maxschaller@users.noreply.github.com>
2021-09-12 12:04:56 -0700

    Added timing to example_test.py.

--- example_test.py
+++ example_test.py
@@ -2,6 +2,7 @@
 from CPG_code.cpg_solver import cpg_solve
 import numpy as np
 import pickle
+import time
 
 # load the serialized problem formulation
 with open('CPG_code/problem.pickle', 'rb') as f:
@@ -14,12 +15,18 @@ prob.param_dict['b'].value = np.random.randn(3,)
 prob.param_dict['c'].value = np.random.rand()
 
 # solve problem conventionally
+t0 = time.time()
 val = prob.solve()
+t1 = time.time()
+print('\nPython solve time:', 1000*(t1-t0), 'ms')
 print('Python solution: x = ', prob.var_dict['x'].value)
 print('Python objective function value:', val)
 
 # solve problem with C code via python wrapper
 prob.register_solve('CPG', cpg_solve)
+t0 = time.time()
 val = prob.solve(method='CPG')
+t1 = time.time()
+print('\nC solve time:', 1000*(t1-t0), 'ms')
 print('C solution: x = ', prob.var_dict['x'].value)
 print('C objective function value:', val)
