dfe63d593bacdec5599c98ebf5103d91f64d3fb2
Steven Diamond <diamond@cs.stanford.edu>
2023-01-22 13:03:10 -0800

    try with free floating pybind11

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'cmake',
         'cvxpy >= 1.3',
-        'pybind11 >= 2.8.0',
+        'pybind11',
         'osqp >= 0.6.2',
         'scipy >= 1.1.0',
         'numpy >= 1.15',
