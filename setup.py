39e02a8c63b873479c6b0990799ea0eea2f616d5
maximilianschaller <79449026+maxschaller@users.noreply.github.com>
2024-04-21 15:17:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'pybind11 >= 2.8',
         'osqp >= 0.6.2, < 1.0.0',
         'clarabel >= 0.6.0',
-        'scipy >= 1.1.0',
+        'scipy >= 1.1.0, <1.12.0',
         'numpy >= 1.15',
     ],
     extras_require={
