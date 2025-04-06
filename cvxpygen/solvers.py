bc55db085c33d71b30f6f774d41fbedf1987823b
govindchari <govindchari1@gmail.com>
2025-04-06 15:02:22 -0700

    Got qoco working

--- cvxpygen/solvers.py
+++ cvxpygen/solvers.py
@@ -1641,7 +1641,7 @@ class QOCOInterface(SolverInterface):
     # solver settings
     stgs_dynamically_allocated = True
     stgs_requires_extra_struct_type = True
-    stgs_direct_write_ptr = 'qoco_solver->settings'
+    stgs_direct_write_ptr = None
     stgs_reset_function = None
     stgs_names = ['feastol', 'abstol', 'reltol', 'feastol_inacc', 'abstol_inacc',
                       'reltol_inacc', 'maxit']
@@ -1679,13 +1679,13 @@ class QOCOInterface(SolverInterface):
                             f'   QOCOCscMatrix* P = (QOCOCscMatrix*)malloc(sizeof(QOCOCscMatrix));\n'
                             f'   QOCOCscMatrix* A = (QOCOCscMatrix*)malloc(sizeof(QOCOCscMatrix));\n'
                             f'   QOCOCscMatrix* G = (QOCOCscMatrix*)malloc(sizeof(QOCOCscMatrix));\n'
-                            f'   qoco_set_csc(P, {canon_constants["n"]}, {canon_constants["n"]}, Canon_Params.P->nnz, Canon_Params.P->x, Canon_Params.P->p, Canon_Params.P->i);\n'
-                            f'   qoco_set_csc(A, {canon_constants["p"]}, {canon_constants["n"]}, Canon_Params.A->nnz, Canon_Params.A->x, Canon_Params.A->p, Canon_Params.A->i);\n'
-                            f'   qoco_set_csc(G, {canon_constants["m"]}, {canon_constants["n"]}, Canon_Params.G->nnz, Canon_Params.G->x, Canon_Params.G->p, Canon_Params.G->i);\n'
+                            f'{f'   qoco_set_csc(P, {canon_constants["n"]}, {canon_constants["n"]}, {{prefix}}Canon_Params.P->nnz, {{prefix}}Canon_Params.P->x, {{prefix}}Canon_Params.P->p, {{prefix}}Canon_Params.P->i);\n' if indices_obj is not None else 'P = NULL;\n'}'
+                            f'{f'   qoco_set_csc(A, {canon_constants["p"]}, {canon_constants["n"]}, {{prefix}}Canon_Params.A->nnz, {{prefix}}Canon_Params.A->x, {{prefix}}Canon_Params.A->p, {{prefix}}Canon_Params.A->i);\n' if canon_constants["p"] > 0 else 'A = NULL;\n'}'
+                            f'{f'   qoco_set_csc(G, {canon_constants["m"]}, {canon_constants["n"]}, {{prefix}}Canon_Params.G->nnz, {{prefix}}Canon_Params.G->x, {{prefix}}Canon_Params.G->p, {{prefix}}Canon_Params.G->i);\n' if canon_constants["m"] > 0 else 'G = NULL;\n'}'
                             f'   QOCOSettings* qoco_settings = (QOCOSettings*)malloc(sizeof(QOCOSettings));\n'
                             f'   set_default_settings(qoco_settings);\n'
                             f'   {{prefix}}qoco_solver = (QOCOSolver*)malloc(sizeof(QOCOSolver));\n'
-                            f'    qoco_setup({{prefix}}qoco_solver, {canon_constants["n"]}, {canon_constants["m"]}, {canon_constants["p"]}, P, Canon_Params.c, A, Canon_Params.b, G, Canon_Params.h, {canon_constants["l"]}, {canon_constants["nsoc"]}'
+                            f'    qoco_setup({{prefix}}qoco_solver, {canon_constants["n"]}, {canon_constants["m"]}, {canon_constants["p"]}, P, {{prefix}}Canon_Params.c, A, {"NULL" if canon_constants["p"] == 0 else f" {{prefix}}Canon_Params.b"}, G, {"NULL" if canon_constants["m"] == 0 else f" {{prefix}}Canon_Params.h"}, {canon_constants["l"]}, {canon_constants["nsoc"]}'
                             f', {"NULL" if canon_constants["nsoc"] == 0 else "(int *) &{prefix}qoco_q"}, qoco_settings)'
             ),
             'A': ParameterUpdateLogic(
@@ -1712,7 +1712,7 @@ class QOCOInterface(SolverInterface):
         if indices_obj is not None:
             self.parameter_update_structure['P'] = ParameterUpdateLogic(
                 update_pending_logic = UpdatePendingLogic(['P']),
-                function_call = 'update_matrix_data({prefix}qoco_solver, {prefix}Canon_Params.P->x), NULL, NULL)'
+                function_call = 'update_matrix_data({prefix}qoco_solver, {prefix}Canon_Params.P->x, NULL, NULL)'
             )
 
         super().__init__(self.solver_name, n_var, n_eq, n_ineq, indices_obj, indptr_obj, shape_obj,
