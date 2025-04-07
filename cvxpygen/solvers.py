b8c3be1bf02d15d5de6bb8dc52b8864cf8464653
govindchari <govindchari1@gmail.com>
2025-04-06 17:30:31 -0700

    Link with .lib for windows

--- cvxpygen/solvers.py
+++ cvxpygen/solvers.py
@@ -1823,7 +1823,9 @@ class QOCOInterface(SolverInterface):
             5 * indent + "os.path.join('c', 'solver_code', 'lib', 'amd'),\n" +
             5 * indent + "os.path.join('c', 'solver_code', 'lib', 'qdldl', 'include'),"),
             ("license='Apache 2.0'", "license='BSD 3-Clause'"),
-            ('extra_objects=[cpg_lib]', "extra_objects=[cpg_lib, os.path.join(cpg_dir, 'build', 'out', 'libqocostatic.a'), os.path.join(cpg_dir, 'build', 'solver_code', 'lib', 'qdldl', 'out', 'libqdldl.a')]")
+            ("lib_name = 'cpg.lib'", "lib_name = 'cpg.lib'\n" + "    libqoco_name = 'qocostatic.lib'\n" + "    libqdldl_name = 'qdldl.lib'"),
+            ("lib_name = 'libcpg.a'", "lib_name = 'libcpg.a'\n" + "    libqoco_name = 'libqocostatic.a'\n" + "    libqdldl_name = 'libqdldl.a'"),
+            ('extra_objects=[cpg_lib]', "extra_objects=[cpg_lib, os.path.join(cpg_dir, 'build', 'out', libqoco_name), os.path.join(cpg_dir, 'build', 'solver_code', 'lib', 'qdldl', 'out', libqdldl_name)]")
         ]
         read_write_file(os.path.join(code_dir, 'setup.py'),
                         lambda x: multiple_replace(x, setup_replacements))
