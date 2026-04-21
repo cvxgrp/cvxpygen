d7700a634322d99e165d152957cc2609cdd98fe2
maximilianschaller <max@schaller.id>
2026-04-21 11:46:37 -0700

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -21,22 +21,23 @@ A.value_sparse = sp.sparse.coo_array((np.random.randn(3), A.sparse_idx), shape=(
 b.value = np.random.randn(m)
 
 # solve with CVXPY
-t = time.time()
-v_ref = problem.solve(solver=cp.OSQP)
+problem.solve(solver=cp.OSQP)
+t = time.time()  # time second solve for fair comparison
+v_ref = problem.solve(solver=cp.OSQP, eps_abs=1e-3, eps_rel=1e-3, polish=False)  # match code gen settings
 t_ref = time.time() - t
 x_ref = x.value.copy()
-dual_ref = problem.constraints[0].dual_value.copy()
+d_ref = problem.constraints[0].dual_value.copy()
 
 # solve with CVXPYgen
 t = time.time()
 v_gen = problem.solve(method='CPG')  # if only, say, b is updated, set updated_params=['b']
 t_gen = time.time() - t
 x_gen = x.value.copy()
-dual_gen = problem.constraints[0].dual_value.copy()
+d_gen = problem.constraints[0].dual_value.copy()
 
 # print comparison
-print(f'\n\t\t\tCVXPY\t\t\tCVXPYgen')
-print(f'Solve time\t\t{1e3 * t_ref:.2f} ms\t\t\t{1e3 * t_gen:.2f} ms')
-print(f'Objective value\t\t{v_ref:.4f}\t\t\t{v_gen:.4f}')
-print(f'Primal solution\t\t{np.round(x_ref, 4)}\t{np.round(x_gen, 4)}')
-print(f'Dual solution\t\t{np.round(dual_ref, 4)}\t\t{np.round(dual_gen, 4)}')
+print(f'\n\t\tCVXPY\t\tCVXPYgen')
+print(f'Solve time\t{1e3 * t_ref:.2f} ms\t\t{1e3 * t_gen:.2f} ms')
+print(f'Objective\t{v_ref:.4f}\t\t{v_gen:.4f}')
+print(f'Solution\t{np.round(x_ref, 4)}\t{np.round(x_gen, 4)}')
+print(f'Dual solution\t{np.round(d_ref, 4)}\t{np.round(d_gen, 4)}')
